c82e72120cc59a5069511113fd71bc55f0bac624
Koshen WSL Home <koshenko.d@webkontrol.com>
2022-10-18 12:46:11 +0300

    Показывать DJ Debug tool только при debug=true

--- movies_admin/config/urls.py
+++ movies_admin/config/urls.py
@@ -1,8 +1,17 @@
 """URL Configuration."""
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('__debug__/', include('debug_toolbar.urls')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [
+        path('debug/', include(debug_toolbar.urls)),
+        *urlpatterns
+    ]
